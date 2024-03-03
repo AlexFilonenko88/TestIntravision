e510461a9b23e16ad3471040f295839142ceaa02
AlexFilonenko88 <af88_88@mail.ru>
2024-03-03 18:06:36 +0300

    fix

--- local/components/certificate.activation/component.php
+++ local/components/certificate.activation/component.php
@@ -89,7 +89,7 @@ while($ar = $res->GetNext()){
 }
 
 echo "<pre>";
-var_dump($arResult["CERTIFICATES"]["ACTIVATED"]["NAME"]);
+print_r($arResult["CERTIFICATES"]["ACTIVATED"]["NAME"]);
 echo "</pre>";
 // echo "<br>";
 // echo "<pre>";
